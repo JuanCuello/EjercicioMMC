a4e184e25d4b67d2b2a05f9ea024f65790d9607b
eb-juanc <jc.edu.ar@gmail.com>
2020-07-29 13:33:36 -0300

    Fix adapter and pagination listener

---
+++ app/src/main/java/com/todoware/ejerciciomeli/ui/resultsDashboard/ResultRecyclerAdapter.kt
@@ -0,0 +1,62 @@
+package com.todoware.ejerciciomeli.ui.resultsDashboard
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.todoware.ejerciciomeli.R
+import com.todoware.ejerciciomeli.models.Result
+import com.todoware.ejerciciomeli.models.SearchResponse
+
+class ResultRecyclerAdapter(
+    var context: Context
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    var searchResponse: SearchResponse?=null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            RecyclerView.ViewHolder {
+
+        // Resullt element
+        return RecyclerViewViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.item_result_element, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: RecyclerView.ViewHolder,
+        position: Int
+    ) {
+        val viewHolder = holder as RecyclerViewViewHolder
+        val result: Result? = searchResponse?.results?.get(position)
+
+        result?.let {
+            // Picasso is handling the external image fetching
+            Picasso.get().load(result.thumbnail).into(viewHolder.imgViewIcon)
+            viewHolder.txtViewTitle.text = result.title
+            viewHolder.txtViewDescription.text = result.price.toString()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return searchResponse?.results?.size?:0
+    }
+
+    // Set the content
+    fun addContent(newResponse: SearchResponse) {
+         searchResponse = newResponse
+    }
+
+    internal inner class RecyclerViewViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        var imgViewIcon: ImageView = itemView.findViewById(R.id.item_result_img)
+        var txtViewTitle: TextView = itemView.findViewById(R.id.item_result_title)
+        var txtViewDescription: TextView = itemView.findViewById(R.id.item_result_description)
+
+
+    }
+
+}
---
+++ app/src/main/java/com/todoware/ejerciciomeli/ui/resultsDashboard/ResultsListFragment.kt
@@ -0,0 +1,110 @@
+package com.todoware.ejerciciomeli.ui.resultsDashboard
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.todoware.ejerciciomeli.R
+import com.todoware.ejerciciomeli.databinding.FragmentResultsBinding
+import com.todoware.ejerciciomeli.repository.MercadoLibreRepository
+import com.todoware.ejerciciomeli.utils.PaginationListener
+import com.todoware.ejerciciomeli.utils.UiUtils
+
+
+class ResultsListFragment : Fragment() {
+
+    lateinit var binding: FragmentResultsBinding
+    lateinit var layoutManager: LinearLayoutManager
+    lateinit var resultsViewModel: ResultsViewModel
+
+    // safeguard to not show results that doesn't match the search
+    var searchForValue = ""
+    var recyclerAdapter: ResultRecyclerAdapter? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // attach this view model to the activity lyfecycle since this response is needed
+        // by the others fragments as well
+
+        activity?.let { it ->
+            resultsViewModel = ViewModelProvider(it).get(ResultsViewModel::class.java)
+                .apply { setRepository(MercadoLibreRepository()) }
+        }
+
+        var isLoading = false
+        binding = FragmentResultsBinding.inflate(inflater, null, false)
+        layoutManager = LinearLayoutManager(context)
+        recyclerAdapter = recyclerAdapter ?: ResultRecyclerAdapter(requireContext())
+
+
+        binding.content.setLayoutManager(layoutManager)
+        binding.content.setAdapter(recyclerAdapter)
+
+        binding.content.addOnScrollListener(object : PaginationListener(layoutManager) {
+            override fun isLastPage(): Boolean {
+                isLoading = false
+                return isLoading
+            }
+
+            override fun loadMoreItems() {
+                resultsViewModel.loadMore(binding.resultsTextDescription.text as String)
+
+                isLoading = true;
+            }
+
+            override fun isLoading(): Boolean {
+                return isLoading
+            }
+
+        })
+        resultsViewModel.searchResultsData.observe(viewLifecycleOwner, Observer { response ->
+            binding.resultsTextDescription.text = response?.query.toString()
+            if (response != null) {
+
+                // initialize the adapter if null and set the data
+                recyclerAdapter?.let {
+                    it.addContent(response)
+                    it.notifyDataSetChanged()
+
+                }
+            } else {
+                // ErrorView
+            }
+        })
+
+        binding.resultFilterButton.setOnClickListener {
+            findNavController().navigate(R.id.action_b_to_a)
+        }
+
+        binding.resultsEditText.doAfterTextChanged {
+            UiUtils.editTextDebouncer(it, searchForValue, ::startSearch, Handler())
+        }
+
+        return binding.root
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        resultsViewModel.searchResultsData.value?.let {
+            recyclerAdapter?.addContent(it)
+        }
+
+    }
+
+    fun startSearch(search: String) {
+        searchForValue = search
+        resultsViewModel.updateSearchQuery(searchForValue)
+
+    }
+}
